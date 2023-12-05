5efd02313cdb1c38d3bba2753645e678282cb57a
Rhuan <rhuanvictorsoares110@gmail.com>
2023-12-05 10:14:37 -0300

    aleracao

---
+++ a2 (1).py
@@ -0,0 +1,24 @@
+import os 
+os.system('cls')
+
+acesso = {"1234" : "ana", "4321" : "pedro"}
+matricula = input('matricula ')
+
+if matricula in acesso.keys():
+    print('Hello ', acesso.get(matricula))
+else:
+   print('erro, faltam 2 tentativas')
+   matricula = input('matricula ')
+   repetir = 2
+
+   while repetir > 0:        
+        repetir -= 1
+        if matricula in acesso.keys():
+         print('ola ', acesso.get(matricula))
+         break      
+        elif matricula not in acesso.keys() and repetir>0:
+         print('vc tem', repetir, 'tentativas')
+         matricula = input('matricula')
+        elif matricula not in acesso.keys() and repetir==0:
+         print("acabou as tentativas")    
+                 
